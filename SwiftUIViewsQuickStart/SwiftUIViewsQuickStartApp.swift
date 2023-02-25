db5e93027432deb1bcb46a9e12daa754349f3383
Andrew Fairchild <andrew.d.fairchild@gmail.com>
2023-02-25 09:11:04 -0600

    Initial Commit

---
+++ SwiftUIViewsQuickStart/SwiftUIViewsQuickStartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIViewsQuickStartApp.swift
+//  SwiftUIViewsQuickStart
+//
+//  Created by Andrew Fairchild on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIViewsQuickStartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
